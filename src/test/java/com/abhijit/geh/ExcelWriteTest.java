d2a224bca7764fa7c8ee6c3fbbd12d77d5494e74
abhijit sinha <abhijit.sinha@imaginea.com>
2018-06-11 12:43:48 +0530

    Use java-random-generator project for random object creation

--- src/test/java/com/abhijit/geh/ExcelWriteTest.java
+++ src/test/java/com/abhijit/geh/ExcelWriteTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -14,7 +15,7 @@ import com.abhijit.geh.GenericExcelHandler;
 import com.abhijit.geh.marker.IExcelObject;
 import com.abhijit.geh.pojo.Student;
 import com.abhijit.geh.pojo.Teacher;
-import com.abhijit.geh.utils.RandomUtil;
+import com.abhijit.jrg.RandomGenerator;
 
 public class ExcelWriteTest {
 
@@ -27,8 +28,8 @@ public class ExcelWriteTest {
 		LOGGER.info("starting application...");
 
 		Map<String, List<T>> outStudents = new HashMap<>();
-		List<T> students = (List<T>) generateStudets(RandomUtil.randomInteger(10, 100), Student.class);
-		List<T> teachers = (List<T>) generateTeachers(RandomUtil.randomInteger(10, 20), Teacher.class);
+		List<T> students = (List<T>) generateStudets(RandomGenerator.integer(10, 100), Student.class);
+		List<T> teachers = (List<T>) generateTeachers(RandomGenerator.integer(10, 20), Teacher.class);
 
 		outStudents.put("Students", students);
 		outStudents.put("Teachers", teachers);
@@ -43,31 +44,37 @@ public class ExcelWriteTest {
 	@SuppressWarnings("unchecked")
 	private static <T extends IExcelObject> T generateTeacher(Class<T> clz) {
 		Teacher teacher = new Teacher();
-		teacher.setGrade(RandomUtil.randomInteger(4, 10));
-		teacher.setName(RandomUtil.randomString(RandomUtil.randomInteger(5, 10)));
-		teacher.setSchool(RandomUtil.randomWords(RandomUtil.randomInteger(1, 3)));
-		teacher.setSection(RandomUtil.randomCharacter("ABCDEFG"));
-		teacher.setSubject(RandomUtil.randomString(RandomUtil.randomInteger(5, 10)));
+		teacher.setGrade(RandomGenerator.integer(4, 10));
+		teacher.setName(RandomGenerator.string(RandomGenerator.integer(5, 10)));
+		teacher.setSchool(join(RandomGenerator.strings(RandomGenerator.integer(1, 3)), (a, b) -> a + " " + b));
+		teacher.setSection(RandomGenerator.character('A', 'B', 'C', 'D', 'E', 'F', 'G'));
+		teacher.setSubject(RandomGenerator.string(RandomGenerator.integer(5, 10)));
 		return (T) teacher;
 	}
 
 	@SuppressWarnings("unchecked")
 	private static <T extends IExcelObject> T generateStudent(Class<T> clz) {
 		Student student = new Student();
-		int grade = RandomUtil.randomInteger(4, 10);
+		int grade = RandomGenerator.integer(4, 10);
 		int age = grade + 5;
 		student.setGrade(grade);
 		student.setAge(age);
 
-		student.setName(RandomUtil.randomString(RandomUtil.randomInteger(5, 10)));
-		student.setFatherName(RandomUtil.randomString(RandomUtil.randomInteger(5, 10)));
-		
-		student.setSchool(RandomUtil.randomWords(RandomUtil.randomInteger(1, 3)));
-		student.setSection(RandomUtil.randomCharacter("ABCD"));
+		student.setName(RandomGenerator.string(RandomGenerator.integer(5, 10)));
+		student.setFatherName(RandomGenerator.string(RandomGenerator.integer(5, 10)));
 
-		student.setHeight(RandomUtil.randomDouble(3, 5));
-		student.setWeight(RandomUtil.randomDouble(30, 50));
+		student.setSchool(join(RandomGenerator.strings(RandomGenerator.integer(1, 3)), (a, b) -> a + " " + b));
+		student.setSection(RandomGenerator.character('A', 'B', 'C', 'D'));
+
+		student.setHeight(RandomGenerator.number(3, 5));
+		student.setWeight(RandomGenerator.number(30, 50));
 		return (T) student;
+	}
+
+	private static <T> T join(List<T> ts, BinaryOperator<T> accumulator) {
+		return ts.stream()
+		    .reduce(accumulator)
+		    .get();
 	}
 
 	private static <T extends IExcelObject> List<T> generateTeachers(int i, Class<T> clz) {
